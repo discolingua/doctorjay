5e73e023e69599393489ad2d26259f9847410867
Bunny Blake <discolingua@gmail.com>
2022-02-05 14:40:11 -0500

    bird move

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,27 +1,37 @@
 class_name Player
-extends RigidBody2D
+extends KinematicBody2D
 
-const HORIZONTAL_FORCE=10
+export(float) var gravity : float = 1
+export(float) var flap : float = 10
+
+const GRAVITY = 1.5
+const FLAP = 3.0
+const ACCELERATION = 600
+const MAX_SPEED = 100
+const FRICTION = 800
+
+var velocity : Vector2 = Vector2.ZERO
+var lastVelocity : Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass 
 
-func _integrate_forces(s):
-	var lv = s.get_linear_velocity()
-	var i = readMovement()
-	var delta = s.get_step()
-
+func _process(_delta):
+	var _i = readMovement()
 	if Input.is_action_just_pressed("ui_accept"):
-		print("flap")
-	if i.x != 0:
-		lv.x += i.x * delta * HORIZONTAL_FORCE
+		_i.y -= flap
+	if _i.x != 0:
+		lastVelocity = _i
+	_i.y += gravity	
+	velocity = move_and_slide(_i * MAX_SPEED)
+
 
 func readMovement() -> Vector2:
 	var _i = Vector2.ZERO
 	
 	_i.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	_i.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	# _i.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 		
 	# normalize vector fixes fast/distorted diagonals
 	_i = _i.normalized()
